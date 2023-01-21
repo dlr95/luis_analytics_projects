497b368a0c6914caa3260521dac3675db44b8abf
Luis de la Riva <luisdlr@Luiss-MacBook-Air.local>
2023-01-22 00:05:31 +0100

    Added first query

---
+++ group_idealista_prices.sql
@@ -0,0 +1,7 @@
+select 
+	sum(case when price >0 and price <=150 then 1 else 0 end) as '0-150',
+	sum(case when price >150 and price <=300 then 1 else 0 end) as '150-300',
+	sum(case when price >300 and price <=450 then 1 else 0 end) as '300-450',
+	sum(case when price >450 and price <=600 then 1 else 0 end) as '450-600',
+	sum(case when price >600 and price <=750 then 1 else 0 end) as '600-750'
+from Idealista_Data
